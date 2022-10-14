2f9326be72210309bac4edfdecd883860eef5ab9
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2022-10-14 13:22:39 -0700

    Commit basic timer
    Timer just counts down from 10

---
+++ Love Matcher/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer: MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    [SerializeField] Text coundownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        coundownText.text = currentTime.ToString("0");
+
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
