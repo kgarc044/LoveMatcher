60ec9e5a3876f457f46768cef9bc5109b5e260c0
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2022-10-14 14:40:07 -0700

    Almost Completed Prototype
    - Added timer and couple counter
    - Matched couples based off interests

--- Love Matcher/Assets/Scripts/Drag Scripts/UnitCollider.cs
+++ Love Matcher/Assets/Scripts/Drag Scripts/UnitCollider.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UnitCollider : MonoBehaviour
 {
+    public GameObject _canvas;
+
+    private void Start()
+    {
+        _canvas = GameObject.Find("Canvas");
+    }
 
     [SerializeField] GameObject particle1, particle2;
 
@@ -18,15 +25,24 @@ public class UnitCollider : MonoBehaviour
         }
         else if (transform.gameObject.tag == "Dragging" && col.gameObject.tag == "Draggable")
         {
-            transform.SetParent(col.gameObject.transform, true);
-            this.gameObject.GetComponent<Npc>().SetFollow(col.gameObject);
-            transform.gameObject.tag = "Untagged";
-
+            Npc dragging = this.gameObject.GetComponent<Npc>();
+            Npc draggable = col.gameObject.GetComponent<Npc>();
+            if (dragging.interest.name == draggable.interest.name)
+            {
+                transform.SetParent(col.gameObject.transform, true);
+                dragging.SetFollow(col.gameObject);
+                transform.gameObject.tag = "Untagged";
+                _canvas.GetComponent<UI>().CoupleTotal = 1;
+            }
         }
+        //else if (transform.gameObject.tag == "Draggable" && col.gameObject.tag == "Dragging")
+        //{
+
+        //}
             //Debug.Log("Collision!");
 
-            var parti2 = GameObject.Instantiate(particle2);
-            parti2.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        var parti2 = GameObject.Instantiate(particle2);
+        parti2.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             //Destroy();
         //}
     }
