f454de1784f4fc711830bec0e504fd0f3724fbbc
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2022-12-08 12:09:48 -0800

    Working Click and drag
    Hero can be properly launched with friction

---
+++ Love Matcher/Assets/Scripts/SlingshotScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class SlingshotScript : MonoBehaviour
+{
+    public float power = 10f;
+    public Rigidbody2D rb;
+
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
+    Camera cam;
+    Vector2 force;
+    Vector3 startingPoint;
+    Vector3 endPoint;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startingPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startingPoint.z = 15;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            endPoint.z = 15;
+
+            force = new Vector2(Mathf.Clamp(startingPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startingPoint.y - endPoint.y, minPower.y, maxPower.y));
+            rb.AddForce(force * power, ForceMode2D.Impulse);
+        }
+    }
+}
