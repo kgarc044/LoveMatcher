2797b632a10336535d08c64c1495ad22a9180d3f
ImRenaldy <62635523+ImRenaldy@users.noreply.github.com>
2022-12-22 14:44:09 -0800

    created flame trap prefab
    flame trap prefab shoots out flame objects
    not currently in a cone but maybe added in the future

---
+++ Love Matcher/Assets/Scripts/flamethrower.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flamethrower : MonoBehaviour
+{
+    public GameObject flame;
+    public float flameSpeed;
+    public float flamelifetime;
+    bool ha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ha = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (ha == true)
+        {
+            StartCoroutine(flameon());
+        }
+    }
+
+    public IEnumerator flameon()
+    {
+        GameObject f = Instantiate(flame, transform.position, transform.localRotation);
+        f.GetComponent<Rigidbody2D>().velocity = transform.right * flameSpeed;
+        Destroy(f, flamelifetime);
+        ha = false;
+        yield return new WaitForSeconds(.05f);
+        ha = true;
+    }
+}
